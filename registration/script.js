77ba20b6526102a6f455a70812743b0b3cf48af5
Harshith Alfred <harshithalfred@gmail.com>
2024-11-14 19:47:28 +0000

    styles

--- registration/script.js
+++ registration/script.js
@@ -2,33 +2,11 @@ $(document).ready(function() {
     $('#registrationForm').on('submit', function(event) {
         event.preventDefault();
         
-        var formData = $(this).serialize();
-      
-        //for the sake of this example, we will use the same URL from javascript 
-        $.ajax({
-            type: 'POST',
-            url: 'processForm.js',
-            data: formData,
-            success: function(response) {
-                var worker = new Worker('processForm.js');
-                
-                worker.postMessage(response);
-                
-                worker.onmessage = function(event) {
-                    $('#result').html(event.data).fadeIn();
-                };
-            }
-        });
-
-        // actual submission of form using php
-        //            $.ajax({
-            //     url: 'submit.php',
-            //     type: 'post',
-            //     data: $(this).serialize(),
-            //     success: function(response) {
-            //         $('#result').html(response).show();
-            //     }
-            // });
+        var formData = new FormData(this);
         
+        // Simulate form processing
+        setTimeout(() => {
+            $('#result').html('Form submitted successfully!').fadeIn();
+        }, 1000);
     });
 });
